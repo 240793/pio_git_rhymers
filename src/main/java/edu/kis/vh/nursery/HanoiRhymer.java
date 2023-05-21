6b4ede1dce8219696b86941f7cc77081c1e95f97
Emil Sarba <240793@edu.p.lodz.pl>
2023-05-21 18:34:38 +0200

    17. dodanie dokumentacji do klasy HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,25 @@
 package edu.kis.vh.nursery;
 
+/**
+ * klasa pozwala na dodawanie i usuwanie liczb na stos sprawdzając poprawność wieży hanoi
+ * elementy nie spełniające założeń wieży hanoi nie są dodawane.
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
 
+    /**
+     * metoda służy to pozyskania ilości elementów odrzuconych, nie spełniejących założeń wieży hanoi
+     * @return liczba odrzuconych elementów
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * metoda dodaje do stosu nowy element o ile spełnia wymagania wieży hanoi
+     * @param in liczba do umieszczenia na stosie
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
